e0a8c442eb6fb4a24f02a4e8a8cb8f9f1b21ab21
ZNFDev <sonubagdi86267@gmail.com>
2025-02-02 21:46:56 +0530

    Update LocalPlayer.kt

--- app/src/main/java/com/mucheng/mucute/client/game/entity/LocalPlayer.kt
+++ app/src/main/java/com/mucheng/mucute/client/game/entity/LocalPlayer.kt
@@ -3,6 +3,18 @@ package com.mucheng.mucute.client.game.entity
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket
 import org.cloudburstmc.protocol.bedrock.packet.StartGamePacket
 import java.util.UUID
+import org.cloudburstmc.math.vector.Vector2f
+import org.cloudburstmc.math.vector.Vector3f
+import org.cloudburstmc.math.vector.Vector3i
+import org.cloudburstmc.protocol.bedrock.data.*
+import org.cloudburstmc.protocol.bedrock.data.inventory.ItemData
+import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.InventoryActionData
+import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.InventorySource
+import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.InventoryTransactionType
+import org.cloudburstmc.protocol.bedrock.packet.*
+import java.util.*
+import kotlin.math.atan2
+import kotlin.math.floor
 
 @Suppress("MemberVisibilityCanBePrivate")
 class LocalPlayer : Player(0L, 0L, UUID.randomUUID(), "") {
@@ -28,6 +40,25 @@ class LocalPlayer : Player(0L, 0L, UUID.randomUUID(), "") {
     override fun onDisconnect(reason: String) {
         super.onDisconnect(reason)
         reset()
+    }
+
+    fun teleport(session: MuCuteRelaySession, x: Float, y: Float, z: Float) {
+        move(x, y, z)
+        session.clientBound(MovePlayerPacket().apply {
+            runtimeEntityId = this@LocalPlayer.runtimeEntityId
+            position = Vector3f.from(x, y, z)
+            rotation = Vector3f.from(rotationPitch, rotationYaw, 0f)
+            //if (rideEntity != null) {
+			//	ridingRuntimeEntityId = rideEntity!!
+			mode = MovePlayerPacket.Mode.HEAD_ROTATION
+			/*} else {
+				mode = MovePlayerPacket.Mode.NORMAL
+			}*/
+        })
+    }
+
+	fun teleport(session: MuCuteRelaySession, vec3: Vector3f) {
+		teleport(vec3.x, vec3.y, vec3.z)
     }
 
 }
