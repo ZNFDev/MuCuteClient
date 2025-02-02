deaceae9e461921e9d8b44e202e4b32f58d518ce
ZNFDev <sonubagdi86267@gmail.com>
2025-02-02 22:07:20 +0530

    Update ClipModule.kt

--- app/src/main/java/com/mucheng/mucute/client/game/module/Misc/ClipModule.kt
+++ app/src/main/java/com/mucheng/mucute/client/game/module/Misc/ClipModule.kt
@@ -2,27 +2,20 @@ package com.mucheng.mucute.client.game.module.misc
 
 import com.mucheng.mucute.client.game.Module
 import com.mucheng.mucute.client.game.ModuleCategory
-import org.cloudburstmc.protocol.bedrock.data.Ability
-import org.cloudburstmc.protocol.bedrock.data.AbilityLayer
-import org.cloudburstmc.protocol.bedrock.data.PlayerPermission
-import org.cloudburstmc.protocol.bedrock.data.command.CommandPermission
 import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket
-import org.cloudburstmc.protocol.bedrock.packet.PlayerAuthInputPacket
-import org.cloudburstmc.protocol.bedrock.packet.UpdateAbilitiesPacket
 import kotlin.math.cos
 import kotlin.math.sin
 
 class ClipModule : Module("clip", ModuleCategory.Misc) {
 
     override fun onReceived(packet: BedrockPacket): Boolean {
-        //if (isInGame)
+        val player = localPlayer// ?: return false // Ensure localPlayer is not null
+        //val session = session ?: return false // Ensure session is not null
 
-    		val player = localPlayer
-    		val yaw = Math.toRadians(player.rotationYaw.toDouble()).toFloat()
-    
-    		player.teleport(session, player.posX - sin(yaw) * 3f, player.posY + 3f, player.posZ + cos(yaw) * 3f)
-    	}
-        return false
+        val yaw = Math.toRadians(player.rotationYaw.toDouble()).toFloat()
+
+        player.teleport(session, player.posX - sin(yaw) * 3f, player.posY + 3f, player.posZ + cos(yaw) * 3f)
+
+        return false // Ensure a Boolean is returned
     }
-
 }
