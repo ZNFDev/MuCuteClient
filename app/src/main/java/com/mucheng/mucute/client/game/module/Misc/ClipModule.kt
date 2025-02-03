de92d1bbd0463ece3f533a359738f637b086d351
ZNFDev <sonubagdi86267@gmail.com>
2025-02-03 09:29:51 +0530

    Update ClipModule.kt

--- app/src/main/java/com/mucheng/mucute/client/game/module/Misc/ClipModule.kt
+++ app/src/main/java/com/mucheng/mucute/client/game/module/Misc/ClipModule.kt
@@ -6,8 +6,24 @@ import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket
 import kotlin.math.cos
 import kotlin.math.sin
 import com.mucheng.mucute.relay.MuCuteRelaySession
+import org.cloudburstmc.protocol.bedrock.packet.BedrockPacket
+import org.cloudburstmc.protocol.bedrock.packet.StartGamePacket
+import java.util.UUID
+import org.cloudburstmc.math.vector.Vector2f
+import org.cloudburstmc.math.vector.Vector3f
+import org.cloudburstmc.math.vector.Vector3i
+import org.cloudburstmc.protocol.bedrock.data.*
+import org.cloudburstmc.protocol.bedrock.data.inventory.ItemData
+import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.InventoryActionData
+import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.InventorySource
+import org.cloudburstmc.protocol.bedrock.data.inventory.transaction.InventoryTransactionType
+import org.cloudburstmc.protocol.bedrock.packet.*
+import java.util.*
+import kotlin.math.atan2
+import kotlin.math.floor
+import com.mucheng.mucute.relay.MuCuteRelaySession
 
-class ClipModule : Module("clip", ModuleCategory.Misc) {
+class ClipModule : Module("Clip", ModuleCategory.Misc) {
 
     override fun onReceived(packet: BedrockPacket): Boolean {
         val player = localPlayer// ?: return false // Ensure localPlayer is not null
@@ -15,8 +31,27 @@ class ClipModule : Module("clip", ModuleCategory.Misc) {
 
         val yaw = Math.toRadians(player.rotationYaw.toDouble()).toFloat()
 
-        player.teleport(this.session, player.posX - sin(yaw) * 3f, player.posY + 3f, player.posZ + cos(yaw) * 3f)
+        teleport(player.posX - sin(yaw) * 3f, player.posY + 3f, player.posZ + cos(yaw) * 3f)
 
         return false // Ensure a Boolean is returned
+    }
+
+    fun teleport(x: Float, y: Float, z: Float) {
+        localPlayer.move(x, y, z)
+        session.clientBound(MovePlayerPacket().apply {
+            runtimeEntityId = localPlayer.runtimeEntityId
+            position = Vector3f.from(x, y, z)
+            rotation = Vector3f.from(localPlayer.rotationPitch, localPlayer.rotationYaw, 0f)
+            //if (rideEntity != null) {
+			//	ridingRuntimeEntityId = rideEntity!!
+	        mode = MovePlayerPacket.Mode.HEAD_ROTATION
+			/*} else {
+				mode = MovePlayerPacket.Mode.NORMAL
+			}*/
+        })
+    }
+
+	fun teleport(vec3: Vector3f) {
+		teleport(vec3.x, vec3.y, vec3.z)
     }
 }
